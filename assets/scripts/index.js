6f421f87145a0bb7af81e0d1ead6645afe7f8b77
juk-eluca <juk.eluca@gmail.com>
2015-01-10 17:58:27 +0200

    Simple server on express, assets build on gulp and browserify

---
+++ assets/scripts/index.js
@@ -0,0 +1,8 @@
+var moment = require("moment");
+var underscore = require("underscore");
+var d3 = require("d3");
+
+d3.json("articles.json", function(error, json) {
+  if (error) return console.warn(error);
+  debugger
+});
